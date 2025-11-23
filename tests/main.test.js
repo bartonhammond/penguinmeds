8452efb287a29bdf2e62fb2ff6d2869e0c4dcf7d
Barton Hammond <bartonhammond@gmail.com>
2025-11-23 17:04:16 -0600

    Check tabs

--- tests/main.test.js
+++ tests/main.test.js
@@ -3,6 +3,7 @@ const title = "Penguin Meds"
 describe('Penquin Meds', () => {
     beforeAll(async () => {
 	await page.goto('http://localhost:8000/')
+	page.on('console', consoleObj => console.log(consoleObj.text()));
     })
 
     it(`should be titled ${title}`, async () => {
@@ -15,6 +16,17 @@ describe('Penquin Meds', () => {
 	});
 
 	expect(Object.keys(localStorageContent).length).toBe(0);
+    })
+
+    it(`should have 5 tabs`, async() => {
+	const numButtons  = await page.$$eval('.tabs button', (buttons) => {
+	    return buttons.length
+	    /*
+	    let str = buttons.map(button  => button.textContent)
+	    return str
+	    */
+	})
+	expect(numButtons).toBe(5); 
     })
 
 })
