be5049179581e1ad150c521efe38d43afa3de07a
Barton Hammond <bartonhammond@gmail.com>
2025-11-23 18:30:25 -0600

    remove service-worker

--- app.js
+++ app.js
@@ -345,10 +345,3 @@ setTimeout(() => {
     updateUI('nicotine');
     cleanOldData();
 }, 100);
-
-// Register service worker
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js')
-        .then(() => console.log('Service Worker registered'))
-        .catch(err => console.log('Service Worker registration failed:', err));
-}
